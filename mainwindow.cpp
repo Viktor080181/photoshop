36b5bb4010dafd7a25ff01e7cabf944c071061a0
Viktor080181 <lozhnvikt0801@mail.ru>
2024-08-02 21:50:43 +0300

    Added project menu. Closed #3

---
+++ mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionNew_file_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionOpen_File_triggered()
+{
+
+}
+
